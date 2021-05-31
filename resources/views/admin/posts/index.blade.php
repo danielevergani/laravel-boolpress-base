e4472940b596169f41fb8512fe0b78084ea27378
danielevergani <dvergani94@gmail.com>
2021-05-31 16:47:17 +0200

    inserite immagini con faker

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -21,7 +21,7 @@
 	<tbody>
 	@foreach ($posts as $post)
 		<tr>
-			<td><img src="{{$post->image ? $post->image : 'https://via.placeholder.com/200'}}" alt="{{$post->title}}" style="width: 100px"></td>
+			<td><img src="{{$post->image ? asset('storage/' . $post->image) : 'https://via.placeholder.com/200'}}" alt="{{$post->title}}" style="width: 100px"></td>
 			<td>{{$post->title}}</td>
 			<td>{{$post->date}}</td>
 			<td>{!! $post->published ? '<i class="fas fa-eye"></i>' : '<i class="fas fa-eye-slash"></i>'!!}</td>
